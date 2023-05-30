d4597898158d1ed3761cca7307e3770cb4d6040b
Clifford Rodriguez <77303096+automationftw@users.noreply.github.com>
2023-05-30 11:24:03 -0500

    initial commit

--- PowerShell7/Task-01.ps1
+++ PowerShell7/Task-01.ps1
@@ -4,15 +4,18 @@ Import-Module .\dell.apex.psm1 -Force
 $org = 'MyOrg'
 
 # CONNECT TO THE APEX API
-connect-restapi -Uri "apis-us0.druva.com" -Endpoint "phoenix"
+connect-restapi -Uri "https://apis-us0.druva.com" -Endpoint "phoenix"
 
 # GET THE ORG
 $query = get-org -Org $org
 
 
 # BACKUP JOBS FILTERED BY VM AND DATE
+# Backup, Restore, Log Request, Defreeze
+
 $Filters = @(
-    "vmName=vc1-win-01.vcorp.local"
+    "vmName=vc1-win-01.vcorp.local",
+    "status=Successful",
     "fromTime=2023-02-24T00:00:01Z",
     "toTime=2023-05-01T23:59:59Z"
 )
